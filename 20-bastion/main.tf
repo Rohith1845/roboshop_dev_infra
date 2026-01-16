855a014cd77864fb2dc3a68c9812040035588588
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-01-16 16:56:21 +0530

    success

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -15,22 +15,36 @@ resource "aws_instance" "bastion" {
 }
 
 
-resource "aws_iam_role" "admin_policy" {
-    name = "BastionAdminRole"
-    assume_role_policy = jsonencode({
-        "Version": "2012-10-17",
-        "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "*",
-            "Resource": "*"
-        }
-    ]
-    })
-    
+resource "aws_iam_role" "bastion_role" {
+  name = "BastionAdminRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
 }
+
+resource "aws_iam_role_policy" "admin_policy" {
+  name = "AdminAccess"
+  role = aws_iam_role.bastion_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action  = "*"
+      Resource = "*"
+    }]
+  })
+}
+
+
 
 resource "aws_iam_instance_profile" "bastion" {
     name = "bastion"
-    role = aws_iam_role.admin_policy.name
+    role = aws_iam_role.bastion_role.name
 }
