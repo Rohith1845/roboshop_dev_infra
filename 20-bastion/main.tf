0ab075ab8b49aab950bfb8d000fdbe0f14d33c28
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-01-16 16:50:31 +0530

    success

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "bastion" {
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_ids
 
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     user_data = file("bastion.sh")
     tags = merge(
         local.common_tags,{
@@ -11,4 +12,25 @@ resource "aws_instance" "bastion" {
         }
     )
 
+}
+
+
+resource "aws_iam_role" "admin_policy" {
+    name = "BastionAdminRole"
+    assume_role_policy = jsonencode({
+        "Version": "2012-10-17",
+        "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+    })
+    
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+    name = "bastion"
+    role = aws_iam_role.admin_policy.name
 }
