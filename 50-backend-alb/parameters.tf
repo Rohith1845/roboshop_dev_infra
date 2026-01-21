83f06c5360e2a13af0cb79cc67952f91aba177c6
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-01-21 16:30:48 +0530

    success

--- 50-backend-alb/parameters.tf
+++ 50-backend-alb/parameters.tf
@@ -1,5 +1,5 @@
-data "aws_ssm_parameter" "backend_alb_listener_arn" {
+resource "aws_ssm_parameter" "backend_alb_listener_arn" {
     name = "/${var.project_name}/${var.environment}/backend_alb_listener_arn"
-    type = "string"
+    type = "String"
     value = aws_lb_listener.backend_alb.arn
 }
