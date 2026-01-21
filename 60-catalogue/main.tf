d61c67e74bbbbfb70ce9382f210e3c4650472c65
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-01-21 10:52:39 +0530

    success

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -70,6 +70,8 @@ resource "aws_alb_target_group" "catalogue" {
     port = 8080
     protocol = "HTTP"
     vpc_id = local.vpc_id
+    deregistration_delay = 60
+
     health_check {
       healthy_threshold = 2
       interval = 10
@@ -80,3 +82,43 @@ resource "aws_alb_target_group" "catalogue" {
       unhealthy_threshold = 2
     }
 }
+
+resource "aws_launch_template" "catalogue" {
+    name = "${var.project_name}-${var.environment}-catalogue"
+    image_id = aws_ami_from_instance.catalogue.id
+    instance_initiated_shutdown_behavior = "terminate"
+    instance_type = "t3.micro"
+
+    vpc_security_group_ids = [local.catalogue_sg_id]
+
+    tag_specifications {
+        resource_type = "instance"
+
+        tags = merge(
+            local.common_tags,
+            {
+                name = "${var.project_name}-${var.environment}-catalogue"
+            }
+        )
+    }
+
+    tag_specifications {
+        resource_type = "volume"
+
+        tags = merge(
+            local.common_tags,
+            {
+                name = "${var.project_name}-${var.environment}-catalogue"
+            }
+        )
+    }
+
+    tags = merge(
+            local.common_tags,
+            {
+                name = "${var.project_name}-${var.environment}-catalogue"
+            }
+        )
+}
+
+
