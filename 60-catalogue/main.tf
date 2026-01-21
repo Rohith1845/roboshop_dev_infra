c5664355a4fa6256f42293b9796d8a397f38f579
Rohith1845 <krishnamsettyrohith@gmail.com>
2026-01-21 16:58:34 +0530

    success

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -76,6 +76,7 @@ resource "aws_lb_target_group" "catalogue" {
       healthy_threshold = 2
       interval = 5
       matcher = "200-299"
+      path = "/health"
       port = 8080
       protocol = "HTTP"
       timeout = 2
